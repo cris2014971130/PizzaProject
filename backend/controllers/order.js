9476904b2ccd4e7fd60a6662719f2048a0144d9a
Boris-Jr <borisyesi30@gmail.com>
2021-11-25 15:02:09 -0500

    order success

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,12 +1,12 @@
 import order from "../models/order.js";
 
 const saveOrder = async (req, res) => {
-  if (!req.body.amount)
+  if (!req.body.amount || !req.body.pizzaId)
     return res.status(400).send({ message: "Incomplete data" });
 
   const orderSchema = new order({
     userId: req.user._id,
-    pizzaId: req.user._id,
+    pizzaId: req.body.pizzaId,
     amount: req.body.amount,
     status: "Received",
     dbStatus: true,
@@ -18,17 +18,17 @@ const saveOrder = async (req, res) => {
 };
 
 const listOrder = async (req, res) => {
-  const orderlist = await role.find();
+  const orderlist = await order.find();
   return orderlist.length == 0
     ? res.status(400).send({ message: "Empty order list" })
     : res.status(200).send({ orderlist });
 };
 
 const updateStatusOrder = async (req, res) => {
-  if (!req.body.status)
+  if (!req.body.status || !req.body._id)
     return res.status(400).send({ message: "Incomplete data" });
 
-  const orderUpdate = await role.findByIdAndUpdate(req.body._id, {
+  const orderUpdate = await order.findByIdAndUpdate(req.body._id, {
     status: req.body.status,
   });
 
