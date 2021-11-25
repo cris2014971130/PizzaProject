bb5da4974ce9b8eb1d97c78f2b3e5fabdb359138
Boris-Jr <borisyesi30@gmail.com>
2021-11-25 10:37:01 -0500

    db folder created

---
+++ backend/models/order.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: {type: mongoose.Schema.ObjectId, ref: "user"},
+  pizzaId: {type: mongoose.Schema.ObjectId, ref: "pizza"},
+  registerDate: { type: Date, default: Date.now },
+  dbStatus: Boolean,
+});
+
+const role = mongoose.model("orders", orderSchema);
+
+export default role;
