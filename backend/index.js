f9343fb7b56facbc9ce6abe5f725bb69aee25d20
Manu3lB <manuel89885@gmail.com>
2021-11-25 11:20:09 -0500

    update user and role

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import db from "./db/db.js";
+import dotenv from "dotenv";
+import role from "./routes/role.js";
+import user from "./routes/user.js";
+//import order from "./routes/order.js";
+import pizza from "./routes/pizza.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/role", role);
+app.use("/api/user", user);
+//app.use("/api/order", board);
+app.use("/api/pizza", pizza);
+
+app.listen(process.env.PORT, () =>
+  console.log("Backend server running on port: " + process.env.PORT)
+);
+
+db.dbConnection();
